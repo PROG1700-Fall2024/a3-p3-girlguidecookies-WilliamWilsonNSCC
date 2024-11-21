91a3fc0ee10bf3ddc69c13711c44034ca6b07ca1
WilliamEWilson <william_wilson_1990@hotmail.com>
2024-11-21 10:55:04 -0400

    Work in progress

--- A3-P3-GirlGuideCookies.py
+++ A3-P3-GirlGuideCookies.py
@@ -4,13 +4,46 @@
 #               those guides who go above and beyond in their sales efforts. The organizers
 #               want a program to print the guide list and their prizes.
 
-#Student #:     
-#Student Name:  
+#Student #: William Wilson   
+#Student Name: w0457754
+
+#num of guides -int
+#Guides Names -list within the 2D list
+#Boxes sold -list int
+#output names to prices -2D List
+#prizes won are between average and totalBoxSold
+
+# xGuides = []
+# boxSold = []
+
+# def girlGuides(xGuides, boxSold):
+def girlGuides():
+    xGuides = []
+    boxSold = []
+    numOfGuides = int(input("Please input the number of Guides:  "))
+
+    for i in range(numOfGuides):
+        while True:
+            nameOfGuides = input(f"Please enter the name of Guild # {i+1}: ")
+            if nameOfGuides.isalpha():
+                break
+            else:
+                print("You entered an invalid entry, please try again!")
+
+        while True:
+            guideSales = input(f"Please enter the number of boxes sold by guide {nameOfGuides}: ")
+            if guideSales.isnumeric():
+                break
+            else:
+                print("You did not enter a number!")
+
+        xGuides.append(nameOfGuides)
+        boxSold.append(guideSales)
 
 def main():
     # YOUR CODE STARTS HERE, each line must be indented (one tab)
 
-
+    girlGuides()
 
 
 
